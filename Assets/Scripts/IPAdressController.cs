f135548d985533c1127cbcb3ae2f5c2234606970
Ellian <62099455+dbsrn0125@users.noreply.github.com>
2025-05-28 01:21:51 +0900

    UI 작업

--- Assets/Scripts/IPAdressController.cs
+++ Assets/Scripts/IPAdressController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using Unity.Robotics.ROSTCPConnector;
+using System;
 public class IPAdressController : MonoBehaviour
 {
     [Header("UI Connection")]
     public TMP_InputField ipAddressInputField;
     public ROSConnection rosConnectionPrefab;
+    public Image statusIndicatorImage;
+
+    [Header("Status Colors")]
+    public Color connectedColor = Color.green;
+    public Color errorColor = Color.red;
+    public Color disconnectedColor = new Color(0.5f, 0.5f, 0.5f, 1f);
     // Start is called before the first frame update
     public void OnClickIpAdressChange()
     {
         rosConnectionPrefab.RosIPAddress = ipAddressInputField.text;
-        Debug.Log("1");
+        UpdateStatusColor();
+    }
+
+    private void Update()
+    {
+        UpdateStatusColor();
+    }
+    private void UpdateStatusColor()
+    {
+        if(rosConnectionPrefab.HasConnectionThread)
+        {
+            if(rosConnectionPrefab.HasConnectionError)
+            {
+                statusIndicatorImage.color = errorColor;
+            }
+            else
+            {
+                statusIndicatorImage.color = connectedColor;
+            }
+        }
+        else
+        {
+            statusIndicatorImage.color = disconnectedColor;
+        }
     }
 }
